c822d836099aef511a723fde7432268f6f796d6e
fgonzales16 <francisgonzales@francis.local>
2016-10-24 18:06:24 +0800

    Revert "test"
    This reverts commit 6b846c70c4bed1363de4c9ce5d61377593c706a7.

--- lib/philippines.rb
+++ lib/philippines.rb
@@ -3,8 +3,18 @@ require "yaml"
 
 module Philippines
   
-  
+  REGION_DATA = YAML.load_file(File.join(File.dirname(__FILE__),"philippines", "data", "regions_provinces.yml"))
   REGION_PROVINCE = YAML.load_file(File.join(File.dirname(__FILE__),"philippines", "data", "regions_province_select.yml"))
-  
+  REGIONS = REGION_DATA.keys
+
+  PROVINCE_DATA = YAML.load_file(File.join(File.dirname(__FILE__),"philippines", "data", "provinces_municipalities.yml"))
+
+  PROVINCES = PROVINCE_DATA.keys.sort
+
+  CITIES = PROVINCE_DATA.map { |_, v| v["cities"] }.flatten.compact.sort
+  MUNICIPALITIES = PROVINCE_DATA.map { |_, v| v["municipalities"] }.flatten.compact.sort
+  MUNICIPALITIES_WITH_PROVINCE = PROVINCE_DATA.map do |province, v| 
+    v["municipalities"].map { |m| "#{m}, #{province}"}
+  end.flatten.sort
 
 end
